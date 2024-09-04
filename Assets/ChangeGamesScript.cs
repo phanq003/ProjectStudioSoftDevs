756bd01bdee41aabbe80e6a4c2fd45d6c8f0b15f
Nam Pham <phanq003@mymail.unisa.edu.au>
2024-09-04 17:02:52 +0930

    feat: add more advanced game select
    games now "shuffle" around, with the next/previous game in the list being displayed on either side

--- Assets/ChangeGamesScript.cs
+++ Assets/ChangeGamesScript.cs
@@ -7,35 +7,63 @@ public class ChangeGamesScript : MonoBehaviour
     public GameObject[] miniGames;
     public int currentGame = 0;
 
-    //TODO Vars for positions of each game? 3 pos i think
+    public Vector3 gamePosLeft = new Vector3(-3.6f, 0, 0);
+    public Vector3 gamePosCentre = new Vector3(0, 0, 0);
+    public Vector3 gamePosRight = new Vector3(3.6f, 0, 0);
+    
+
 
     void Start() //Unsure if should do this, keep everything visible in first place instead?
     {
-        miniGames[currentGame].SetActive(true);
-
+        displayGames();
     }
     public void nextGame(){
-        miniGames[currentGame].SetActive(false);
         currentGame = (currentGame + 1) % miniGames.Length; //Modulo allows for it to loop back to 0 once limit reached
-        Debug.Log(currentGame);
+        Debug.Log("Current Game Index: " + currentGame);
 
-        miniGames[currentGame].SetActive(true);
+        displayGames();
     }
 
     public void prevGame(){
-        miniGames[currentGame].SetActive(false);
         currentGame -= 1;
         if (currentGame < 0){
             currentGame = miniGames.Length-1; //TODO Check if this works as intended
         }
-        Debug.Log(currentGame);
+        Debug.Log("Current Game Index: " + currentGame);
 
-        miniGames[currentGame].SetActive(true);
+        displayGames();
 
     }
 
     public void selectGame(){
 
+    }
+
+    private void displayGames(){
+        //Hiding and unhiding neccasary games
+        for (int i = 0; i < miniGames.Length; i++){
+            if (i == currentGame || i == (currentGame + 1) % miniGames.Length || i == currentGame-1){ // Checks for the 3 games being displayed
+              miniGames[i].SetActive(true);
+            }
+            else if (currentGame-1 < 0 && i == miniGames.Length-1){ //Checks for edge case where "left" game would be the last game in list
+                miniGames[i].SetActive(true);
+            }
+            else {
+                miniGames[i].SetActive(false);
+            }
+        }
+
+        //Setting positions
+    //TODO Check if this should be in the for loop
+        miniGames[currentGame].transform.position = gamePosCentre;
+        miniGames[(currentGame + 1) % miniGames.Length].transform.position = gamePosRight;
+        if (currentGame-1 < 0){
+            miniGames[miniGames.Length-1].transform.position = gamePosLeft;
+        }
+        else {
+            miniGames[currentGame-1].transform.position = gamePosLeft;
+
+        }
     }
 
 }

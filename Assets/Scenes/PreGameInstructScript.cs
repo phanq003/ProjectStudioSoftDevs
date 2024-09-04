5a808d44797dd99eb8ac8b3a319b72582a7f1646
RileyDOnise <79576716+RileyDOnise@users.noreply.github.com>
2024-09-05 02:46:56 +0930

    added a preGameInstructions
    The gameSelect scene is currently linked to both the play and the back button for testing. however this will be linked to the game when it is possible. a basic layout has been done but could use some refinement

---
+++ Assets/Scenes/PreGameInstructScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PreGameInstructScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame()
+    {
+        Debug.Log("the button is being pressed");
+        SceneManager.LoadScene("GameSelectScene");
+    }
+}
+

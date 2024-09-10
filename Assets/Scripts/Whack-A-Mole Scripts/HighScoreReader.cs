8201b46b3d080638aac18c760f5d4ea43bcdc794
hutbj003 <hutbj003@mymail.unisa.edu.au>
2024-09-10 09:54:13 +0930

    Fix: Fixed the array not saving users highscore

--- Assets/Scripts/Whack-A-Mole Scripts/HighScoreReader.cs
+++ Assets/Scripts/Whack-A-Mole Scripts/HighScoreReader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -33,31 +34,47 @@ public class HighScoreReader : MonoBehaviour
     [System.Serializable]
     public class HighScoreList
     {
-        public HighScorePlayer[] HighScorePlayerList;
-        private HighScorePlayer[] tenPlayerList;
+        public List<HighScorePlayer> HighScorePlayerList = new List<HighScorePlayer>();
+        private List<HighScorePlayer> tenPlayerList = new List<HighScorePlayer>();
 
         public void limitPlayersToTen()
         {
+            try
+            {
+                Debug.Log(HighScorePlayerList.Count());
+            }
+            catch
+            {
+
+            }
             int count = HighScorePlayerList.Count();
+            Debug.Log("count" + count);
             if (count != 0)
             {
+                tenPlayerList = new List<HighScorePlayer>();
                 HighScorePlayerList.OrderByDescending(sc => sc.score).ToList();
-            }
-            if (count > numOfPlayers)
-            {
                 foreach (HighScorePlayer thePlayers in HighScorePlayerList)
                 {
-                    bool hasAppend = false;
-                    while (tenPlayerList.Count() < 10 || hasAppend == false)
+                    if (count > numOfPlayers)
                     {
-                        tenPlayerList.Append(thePlayers);
-                        hasAppend = true;
+                        bool hasAppend = false;
+                        while (tenPlayerList.Count() < 10 || hasAppend == false)
+                        {
+                            tenPlayerList.Add(thePlayers);
+                            hasAppend = true;
+                        }
+                    }
+                    else
+                    {
+                        tenPlayerList.Add(thePlayers);
                     }
                 }
             }
             
+          
+            
         }
-        public HighScorePlayer[] getPlayerList()
+        public List<HighScorePlayer> getPlayerList()
         {
             return tenPlayerList; 
         }
@@ -67,7 +84,13 @@ public class HighScoreReader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerList = JsonUtility.FromJson<HighScoreList>("");
+        try
+        {
+            playerList = JsonUtility.FromJson<HighScoreList>(Application.dataPath + "/scoreWack.text");
+        }
+        catch {
+            writeToList();
+        }
         
        
     }
@@ -77,15 +100,25 @@ public class HighScoreReader : MonoBehaviour
     {
         
     }
+    public void writeToList()
+    {
+        print("WriteTOList" + playerList.getPlayerList().ToString());
+        print(playerList.getPlayerList().Count() + "HJDASKHSDADJLSJKDKSADJDLKSLKDJSALKDJAYOOOOO");
+        string jsonFileString = JsonUtility.ToJson(playerList.getPlayerList());
+        File.WriteAllText(Application.dataPath + "/scoreWack.text", jsonFileString);
+    }
     public void saveScore()
     {
-        string playerName = playerInput.text;
+        print("saved" + playerInput.text + currentScoreText.text);
         HighScorePlayer newPlayer = new HighScorePlayer(playerInput.text, int.Parse(currentScoreText.text));
+        playerList.HighScorePlayerList.Add(newPlayer);
+        print(playerList.HighScorePlayerList.Count() + "SAKJDKDJLSAKDKJLSJALKD");
         playerList.limitPlayersToTen();
+        this.writeToList();
     }
     public void displayList()
     {
-        if (playerList != null)
+        if (playerList.getPlayerList().Count() != 0)
         {
             float offset = 0f;
             int cycle = 1;

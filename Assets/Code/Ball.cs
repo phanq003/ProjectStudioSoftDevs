1392a52e0e66e2513456820cce2957cba7fbf956
hutbj003 <hutbj003@mymail.unisa.edu.au>
2024-09-11 22:11:02 +0930

    Feat (SOF-107): Added a Ball Indicator to the game
    Add a visual that scales with power in the direction that the player will shoot the ball in the golf minigame scene

--- Assets/Code/Ball.cs
+++ Assets/Code/Ball.cs
@@ -9,15 +9,19 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public LogicScript logic;
+    public GameObject golfBallIndicator;
     private float forceMulitplyer = 5f;
     private Rigidbody2D rb;
     private Vector2 startMousePos;
     private Vector2 endMousePos;
+    private Vector2 shotStartLocation;
     private bool isDragging = false;
+    private float offset = 1.3f;
 
 
     void Start()
     {
+        golfBallIndicator.SetActive(false);
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,12 +29,30 @@ public class Ball : MonoBehaviour
     {
         if (logic.getPlayerTurnExceeded() == false)
         {
+            UnityEngine.Debug.Log(shotStartLocation);
+            UnityEngine.Debug.Log(startMousePos);
+            if (Input.GetMouseButtonDown(0))
+            {
+                golfBallIndicator.SetActive(true);
+                shotStartLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
             if (Input.GetMouseButton(0))
             {
                 Console.Out.WriteLine("Mouse button pressed");
                 startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Console.Out.WriteLine(startMousePos);
+                //UnityEngine.Debug.Log(startMousePos);
                 isDragging = true;
+                //Vector2 ballLocation = gameObject.transform.position;
+               
+                float scaleVal = Vector2.SqrMagnitude(shotStartLocation - startMousePos);
+                Vector3 testVect = (shotStartLocation - startMousePos).normalized * scaleVal /30;
+                Vector3 direction = (shotStartLocation - startMousePos);
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                golfBallIndicator.transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+                golfBallIndicator.transform.localScale = new Vector3(scaleVal / 40, scaleVal / 4, 1);
+                    //golfBallIndicator.transform.position = gameObject.transform.position + new Vector3(offset * scaleVal / 40, offset * scaleVal / 40, 0).normalized;
+                    golfBallIndicator.transform.position = gameObject.transform.position + testVect;
             }
 
             if (Input.GetMouseButtonDown(0) && isDragging)
@@ -44,6 +66,7 @@ public class Ball : MonoBehaviour
                 rb.AddForce(-direction * forceMulitplyer, ForceMode2D.Impulse);
                 isDragging = false;
                 logic.addStroke();
+                golfBallIndicator.SetActive(false);
             }
         }
     }

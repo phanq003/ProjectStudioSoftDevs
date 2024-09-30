0e609eb443588ea6c1ae09b147123aaf2973b9eb
donry019 <donry019@mymail.unisa.edu.au>
2024-10-01 01:32:34 +1000

    FEAT(SOF-222) Improved template for pre game instructions

---
+++ Assets/Scenes/PlayButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void play()
+    {
+        SceneManager.LoadScene(""); //enter game scene name here
+    }
+
+
+}

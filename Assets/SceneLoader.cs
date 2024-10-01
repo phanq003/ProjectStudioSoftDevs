4979385be28039c517da4f01d31ab3efb1b1d162
donry019 <donry019@mymail.unisa.edu.au>
2024-10-01 23:12:06 +0930

    FEAT(SOF-94) added graphics and enter detection
    Just need to add pathing to what happens when enter is pressed

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string sceneName;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Debug.Log("Enter is pressed");
+           SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}

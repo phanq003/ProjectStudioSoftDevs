9af5cac89289638b6dc437e2fa605b42749191c1
John Patrick Lim <limjy070@mymail.unisa.edu.au>
2024-10-01 15:19:17 +0930

    refactor: update score game over feedback text

--- Assets/Scripts/Recall-It Scripts/RecallGameUIScript.cs
+++ Assets/Scripts/Recall-It Scripts/RecallGameUIScript.cs
@@ -26,8 +26,8 @@ public class RecallGameUIScript : MonoBehaviour
     private bool isPaused;
     private bool isGameOver;
     private float durationButtonDelay = 1;
-    private string highScoreBeatenComment = "You beaten the High Score!";
-    private string regularResultComment = "Awesome! You did great!";
+    private string highScoreBeatenComment = "You beat the High Score! Enter your name for show!";
+    private string regularResultComment = "Awesome! Enter your name to save your score!";
     [SerializeField] Image correctImage;
     [SerializeField] Image wrongImage;
     private float durationFeedbackDelay = 1;

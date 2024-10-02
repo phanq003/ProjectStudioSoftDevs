f6c370bada3e21095bd89aa16bcfc3f09b9a4a91
hutbj003 <hutbj003@mymail.unisa.edu.au>
2024-10-03 02:30:53 +0930

    Chore: Fixing Hole1 Scene regarding merge

---
+++ Assets/Scripts/Golf Scripts/GameTracker.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameTracker : MonoBehaviour
+{
+    public int strokeCounter = 0;
+    public int holeCounter = 1;
+    public Text strokeText;
+
+
+
+}

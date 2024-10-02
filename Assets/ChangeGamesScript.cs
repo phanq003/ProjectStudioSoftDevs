a99be3d34f88c4c193c4e3b226d36b124b223b45
Nam Pham <phanq003@mymail.unisa.edu.au>
2024-10-02 16:39:46 +0930

    feat: SOF-37 add standard sprites to UI

--- Assets/ChangeGamesScript.cs
+++ Assets/ChangeGamesScript.cs
@@ -52,10 +52,10 @@ public GameObject popup;
 
     public void onSelectGame(){
         selectedGame = miniGames[currentGame];
-        showPopup();
+        showPopup(); //TODO CHANGEEEEEEEEEEE!!
     }
 
-    private void showPopup(){
+    private void showPopup(){ 
         Debug.Log(popupControl.name);
         popupControl.setSelectedGame(selectedGame);
         popupControl.showPopup();
